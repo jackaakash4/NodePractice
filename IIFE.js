62648fdc16245a583487abff5ef1484e7477f8da
Jack Aakash <jackaakash4@gmail.com>
2023-08-08 08:12:29 +0545

    IIFE

---
+++ IIFE.js
@@ -0,0 +1,9 @@
+(function(message){
+    const superHero = 'Batman';
+    console.log(message, superHero);
+})('I am ');
+
+(function(message){
+    const superHero = 'Superman';
+    console.log(message, superHero);
+})('hey');
--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-require('./batman')
-require('./superman')
+const SuperHero = require('./super-hero');
+
+const batman = new SuperHero("Batman");
+console.log(batman.getName());
+
+batman.setName('Bruce wayne');
+console.log(batman.getName());
+
+const superman = new SuperHero("Superman");
+console.log(superman.getName());
